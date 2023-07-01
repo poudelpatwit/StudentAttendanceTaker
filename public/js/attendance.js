43477ffa9f476c652a94d572fab1bae7452fd1da
poudelpatwit <prafulpoudel21@gmail.com>
2023-07-01 12:43:07 -0400

    Code to scan attendance

---
+++ public/js/attendance.js
@@ -0,0 +1,105 @@
+document.getElementById('submit-attendance').addEventListener('click', async (event) => {
+    // prevent form from submitting normally
+    event.preventDefault();
+
+    // function to get user's location
+    const getLocation = () => {
+        return new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(resolve, reject);
+        });
+    }
+
+    //function to scan the qr code data
+    const getQRCode = () => {
+        return new Promise((resolve, reject) => {
+            // assuming videoElement is the video where the QR code will be shown
+            let videoElement = document.getElementById('qr-video');
+
+            navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } })
+                .then((stream) => {
+                    videoElement.srcObject = stream;
+                    videoElement.setAttribute("playsinline", true);
+                    videoElement.play();
+
+                    videoElement.onloadedmetadata = () => {
+                        let canvasElement = document.createElement('canvas');
+                        canvasElement.width = videoElement.videoWidth;
+                        canvasElement.height = videoElement.videoHeight;
+                        let canvas = canvasElement.getContext('2d');
+
+                        setInterval(() => {
+                            canvas.drawImage(videoElement, 0, 0, canvasElement.width, canvasElement.height);
+                            let imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
+                            let code = jsQR(imageData.data, imageData.width, imageData.height);
+
+                            if (code) {
+                                resolve(code.data);
+                            }
+                        }, 500);
+                    }
+                })
+                .catch((error) => {
+                    reject(error);
+                });
+        });
+    }
+
+
+
+
+    try {
+        // get the location data
+        const position = await getLocation();
+
+        // create data for the QR code, which includes the location
+        let locationData = {
+            // include other data you want in the QR code
+            location: {
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude,
+            }
+        };
+
+        const qrCodeData = await getQRCode();
+
+
+        console.log("Location Data: ", locationData)
+        console.log("QR Code Data: ", qrCodeData)
+
+
+        const attendanceData = {
+            location: {
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude,
+            },
+            qrCodeData: qrCodeData
+        }
+
+
+        const jsonAttendanceData = JSON.stringify(attendanceData);
+
+        // send your data to the server for verification
+        let response = await fetch('https://enormous-oil-speedwell.glitch.me/verify', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: jsonAttendanceData
+        });
+
+        // check response status
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        } else {
+            // if everything is ok, you can parse and use the response data
+            let result = await response.json();
+            console.log(result);
+        }
+    } catch (error) {
+        console.log('There was a problem with the fetch operation: ' + error.message);
+    }
+});
+
+
+
+
